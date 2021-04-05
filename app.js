d707f938052e5df4bb1e99dc278da781a0c9483d
Mr-jhownes <jhon_jonathan_cs@hotmail.com>
2021-04-05 15:27:34 +0000

    correcting appjs

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 //including modules, for its funcionalities
-<<<<<<< HEAD
-const http = require('http'), 
-=======
 var http = require('http'), 
->>>>>>> 61da4537dce292f5f4b2a34c0dbbd1f48517de9f
     path = require('path'),
     express = require('express'),
     fs = require('fs'),
